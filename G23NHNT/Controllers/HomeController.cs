b2ce5fb46a7a8b44887488835cc7a9b8b693ee92
TusnCoder19 <21a100100409@students.hou.edu.vn>
2024-11-17 23:11:45 +0700

    xử lí danh mục

--- G23NHNT/Controllers/HomeController.cs
+++ G23NHNT/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
-﻿// HomeController.cs
-using G23NHNT.Models;
+﻿using G23NHNT.Models;
 using G23NHNT.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace G23NHNT.Controllers
@@ -10,35 +9,63 @@ namespace G23NHNT.Controllers
     public class HomeController : Controller
     {
         private readonly IHouseRepository _houseRepository;
+        private readonly IHouseTypeRepository _houseTypeRepository;
+        private readonly G23_NHNTContext _context;
 
-        public HomeController(IHouseRepository houseRepository)
+        public HomeController(IHouseRepository houseRepository, IHouseTypeRepository houseTypeRepository, G23_NHNTContext context)
         {
             _houseRepository = houseRepository;
+            _houseTypeRepository = houseTypeRepository;
+            _context = context;
         }
 
         public async Task<IActionResult> Index()
         {
             var houses = await _houseRepository.GetAllHousesAsync();
-
+            var houseTypes = await _houseTypeRepository.GetAllHouseTypes();
             var viewModel = new HomeViewModel
             {
                 Houses = houses,
-                IsChuTro = User.IsInRole("ChuTro") 
+                HouseTypes = houseTypes,
+                IsChuTro = User.IsInRole("ChuTro"),
             };
 
             return View(viewModel);
         }
 
+        public async Task<IActionResult> HousesByType(int id)
+        {
+            var houses = await _houseRepository.GetHousesByTypeAsync(id);
+
+            if (houses == null || !houses.Any())
+            {
+                return NotFound(); 
+            }
+            var selectedHouseType = await _context.HouseType.FirstOrDefaultAsync(ht => ht.IdHouseType == id);
+            if (selectedHouseType == null)
+            {
+                return NotFound();
+            }
+            var houseTypes = await _houseTypeRepository.GetAllHouseTypes();
+
+            var viewModel = new HomeViewModel
+            {
+                Houses = houses,
+                HouseTypes = houseTypes,
+                SelectedHouseType = selectedHouseType,
+                IsChuTro = User.IsInRole("ChuTro"),
+            };
+            
+            return View("HousesListPartial", viewModel);
+        }
         public async Task<IActionResult> Detail(int id)
         {
-           
-                var house = await _houseRepository.GetHouseWithDetailsAsync(id);
-                if (house != null)
-                {
-                    return View("HouseDetails", house);
-                }
+            var house = await _houseRepository.GetHouseWithDetailsAsync(id);
+            if (house != null)
+            {
+                return View("HouseDetails", house);
+            }
             return NotFound();
         }
-
     }
 }
--- G23NHNT/Models/HomeViewModel.cs
+++ G23NHNT/Models/HomeViewModel.cs
@@ -6,7 +6,10 @@ namespace G23NHNT.Models
 {
     public class HomeViewModel
     {
+        public IEnumerable<HouseType>? HouseTypes { get; set; }
+        public HouseType? SelectedHouseType { get; set; }    
+
         public IEnumerable<House> Houses { get; set; }
-        public bool IsChuTro { get; set; } 
+        public bool IsChuTro { get; set; }
     }
 }
